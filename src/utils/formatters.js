ba692bc31c3eab80bec981c76197fa38808f350d
thang2k6adu <thang2k6adu@gmail.com>
2025-04-24 23:20:34 +0700

    [FIX] BoardContent empty column

--- src/utils/formatters.js
+++ src/utils/formatters.js
@@ -3,3 +3,13 @@ export const capitalizeFirstLetter = (val) => {
   return `${val.charAt(0).toUpperCase()}${val.slice(1)}`
 }
 
+// Hàm tạo ra placeholder card
+// với id có dạng columnId-placeholder-card
+export const generatePlaceholderCard = (column) => {
+  return {
+    _id: `${column._id}-placeholder-card`,
+    boardId: column.boardId,
+    columnId: column._id,
+    FE_PlaceholderCard: true,
+  }
+}
