4d2b38c190cbcfff045280d5e07c65a0c7b5bc65
thang2k6adu <thang2k6adu@gmail.com>
2025-05-18 21:34:38 +0700

    fix BUILD_MODE production

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,8 @@
-// export const API_ROOT = 'http://localhost:3000'
-export const API_ROOT = 'https://trello-api-4hev.onrender.com'
+let apiRoot = ''
+if (process.env.BUILD_MODE === 'dev') {
+  apiRoot = 'http://localhost:3000'
+}
+if (process.env.BUILD_MODE === 'production') {
+  apiRoot = 'https://trello-api-4hev.onrender.com'
+}
+export const API_ROOT = apiRoot
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react-swc'
 import svgr from 'vite-plugin-svgr'
 // https://vitejs.dev/config/
 export default defineConfig({
+  // Cho phép Vite sử dụng process.env (mặc định nó sẽ không hiểu và phải dùng import.meta.env)
+  define: {
+    'process.env': process.env,
+  },
   plugins: [react(), svgr()],
   // base: './'
   resolve: {
