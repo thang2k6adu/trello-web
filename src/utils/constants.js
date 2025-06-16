4570d7c92f358f522f4d22711eb6b321ba865a9a
thang2k6adu <thang2k6adu@gmail.com>
2025-06-16 13:59:25 +0700

    UPDATE apiRoot

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,8 +1,8 @@
 let apiRoot = ''
 if (process.env.BUILD_MODE === 'dev') {
-  apiRoot = 'http://localhost:3000'
+  apiRoot = 'http://localhost:3000/v1'
 }
 if (process.env.BUILD_MODE === 'production') {
-  apiRoot = 'https://condemned-jeanie-thang2k6adu-87c8a0dd.koyeb.app'
+  apiRoot = 'https://condemned-jeanie-thang2k6adu-87c8a0dd.koyeb.app/v1'
 }
 export const API_ROOT = apiRoot
